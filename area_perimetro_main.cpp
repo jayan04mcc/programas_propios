51b7d1dad7bff5731039f8d427e18984d0133e50
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2022-05-12 20:00:31 -0500

    Create area_perimetro_main.cpp

---
+++ area_perimetro_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "area_y_perimetro.h"
+using namespace std;
+
+int main()
+{
+    area_y_perimetro prueba1=area_y_perimetro(4,6);
+    prueba1.area();
+    return 0;
+}
