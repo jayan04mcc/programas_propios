15abb5d9b8cb93fa53ce2fb89537adf437c71879
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2022-05-12 19:59:39 -0500

    Create area_perimetro_clase.h

---
+++ area_perimetro_clase.h
@@ -0,0 +1,20 @@
+#ifndef AREA_Y_PERIMETRO_H
+#define AREA_Y_PERIMETRO_H
+
+
+class area_y_perimetro
+{
+private:
+    int base;
+    int altura;
+    public:
+        area_y_perimetro(int,int);
+        ~area_y_perimetro();
+        void area();
+        void perimetro();
+
+
+
+};
+
+#endif // AREA_Y_PERIMETRO_H
